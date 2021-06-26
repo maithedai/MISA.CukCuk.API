4a6b7f2e5cac9ee11f30a5bf0f8168ef5094f6cb
maithedai <daibengi19@gmail.com>
2021-06-26 08:05:00 +0700

    aa

---
+++ MISA_CukCuk_Test_API/MISA_CukCuk_Test_API/Controllers/CustomersController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MISA_CukCuk_Test_API.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using MySqlConnector;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MISA_CukCuk_Test_API.Controllers
+{
+    [Route("api/v1/customers")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {     
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var connectionString = "User Id=dev;Host=47.241.69.179;Port=3306;Database='MISACukCuk_Demo';Password=12345678;Character Set=utf8";
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var customers = dbConnection.Query<Customer>("Proc_GetCustomers", commandType: CommandType.StoredProcedure);
+            return Ok(customers);
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            var connectionString = "User Id=dev;Host=47.241.69.179;Port=3306;Database='MISACukCuk_Demo';Password=12345678;Character Set=utf8";
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var customers = dbConnection.Query<Customer>("Proc_GetCustomerById", new { CustomerId = id }, commandType: CommandType.StoredProcedure);
+             
+            return Ok(customers);
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public IActionResult Post(Customer customer)
+        {
+            var connectionString = "User Id=dev;Host=47.241.69.179;Port=3306;Database='MISACukCuk_Demo';Password=12345678;Character Set=utf8";
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var rowAffects = dbConnection.Execute("Proc_InsertCustomer", customer, commandType: CommandType.StoredProcedure);
+
+            return Ok(rowAffects);
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ MISA_CukCuk_Test_API/MISA_CukCuk_Test_API/Models/Customer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA_CukCuk_Test_API.Models
+{
+    public class Customer
+    {
+        #region Property
+        /// <summary>
+        /// Khóa chính
+        /// </summary>
+        public Guid CustomerId { get; set; }
+        
+        /// <summary>
+        /// Mã khách hàng
+        /// </summary>
+        public string CustomerCode { get; set; }
+
+        /// <summary>
+        /// Họ và tên
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Ngày sinh
+        /// </summary>
+        public DateTime? DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Địa chỉ
+        /// </summary>
+        public string Aderss { get; set; }
+
+        /// <summary>
+        /// Số điện thoại
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Giới tính
+        /// </summary>
+        public int? Gender { get; set; }      
+
+        /// <summary>
+        /// Địa chỉ Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Ngày tạo
+        /// </summary>
+        public DateTime? CreatedDate { get; set; }
+
+        /// <summary>
+        /// Người tạo
+        /// </summary>
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// Ngày sửa đổi
+        /// </summary>
+        public DateTime? ModifiedDate { get; set; }
+
+        /// <summary>
+        /// Nhóm khách hàng
+        /// </summary>
+        public Guid? CustomerGroupId { get; set; }
+
+        public string MemberCardCode { get; set; }
+
+        public string Node { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string CompanyTaxCode { get; set; }
+
+        #endregion
+
+    }
+}
